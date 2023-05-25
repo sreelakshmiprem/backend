2749340231e1e90c5f25c66df208e2401796a516
Sreelakshmi Prem <sreelakshmiprem27@gmail.com>
2023-05-25 17:02:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,106 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+const mongoose = require("mongoose")
+
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb+srv://sreelakshmi24:sreelakshmi24@sreelakshmiprem24.l4einlx.mongodb.net/project')
+    .then(() => {
+        console.log("DB connected")
+    })
+    .catch(err => {
+        console.log('Error :' + err)
+    })
+
+
+const Studentdata = require('./model/student')
+
+
+//post method
+app.post('/student',async (req,res)=>{
+
+
+    try {
+        let item = req.body
+        console.log('chk',item)
+    
+        const user = new Studentdata(item)
+        const savedStudent = await user.save()
+        res.status(201).send(savedStudent)
+    } catch (error) {
+        console.log(error)
+    }
+
+   
+})
+
+// get data 
+app.get('/student', async(req,res)=>{
+
+    try {
+
+        const list = await Studentdata.find()
+        res.json(list)
+
+        
+    } catch (error) {
+        console.log(error)
+        res.status(400)
+    }
+})
+
+//update
+
+app.put('/student/:id',async (req,res)=>{
+
+    try {
+
+
+        let id = req.params.id
+        let item = req.body
+        let updatedData = {$set:item}
+
+        let updatedStudent = await Studentdata.findByIdAndUpdate({'_id':id}, updatedData,{new:true})
+        res.send(updatedStudent)
+
+
+        
+    } catch (error) {
+
+        console.log(error)
+        res.status(400)
+        
+    }
+})
+
+
+//delete
+
+
+app.delete('/student/:id',async (req,res)=>{
+try {
+
+    let id = req.params.id
+    const deleteStudent = await Studentdata.findByIdAndDelete(id)
+
+    res.send(deleteStudent)
+    
+} catch (error) {
+    console.log(error)
+    
+}
+
+})
+
+
+
+
+
+
+
+app.listen(3200, () => {
+    console.log("Server is running")
+})
